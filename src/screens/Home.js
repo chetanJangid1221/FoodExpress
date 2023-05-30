4566f3c872c3806926e843b9619842fbfc4c527a
Chetan Jangid <104720682+chetanJangid1221@users.noreply.github.com>
2023-05-30 12:17:17 +0530

    Update Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
 
 
     const loadData = async () => {
-        let response = await fetch("/foodData", {
+        let response = await fetch("https://foodexp.onrender.com/foodData", {
             method: "POST",
             headers: {
                 "Content-Type": 'application/json'
