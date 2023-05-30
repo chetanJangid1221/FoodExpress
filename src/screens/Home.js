d083bbb515d5be178a29fde47c961748a90519ac
Chetan Jangid <104720682+chetanJangid1221@users.noreply.github.com>
2023-05-30 16:17:32 +0530

    Update Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -11,7 +11,8 @@ export default function Home() {
 
 
     const loadData = async () => {
-        const response = await fetch('http://localhost:5000/foodData', {
+         const response = await fetch('https://foodexp.onrender.com/foodData', {
+//         const response = await fetch('http://localhost:5000/foodData', {
             mode: 'no-cors',
             method: "POST",
             headers: {
