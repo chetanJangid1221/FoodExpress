3de3052f416587965384138e66f4ab448116cad0
Chetan Jangid <104720682+chetanJangid1221@users.noreply.github.com>
2023-05-31 06:47:42 +0530

    change the server

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,9 @@ const app = express()
 const port = 5000
 const mongoDB = require('./db')
 app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+    // res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin","https://foodexpress-jrwp.onrender.com/");
+
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-Width, Content-Type, Accept"
